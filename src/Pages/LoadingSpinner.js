ac675a8f78e16a1bdfa584015333caf855cacc06
Harrison <hwinslow@student.byu.edu>
2024-04-04 23:08:59 -0600

    Updated loading spinner to also display a message

--- src/Pages/LoadingSpinner.js
+++ src/Pages/LoadingSpinner.js
@@ -2,9 +2,12 @@ import React from 'react';
 import '../Styles/Pages.css'
 
 const LoadingSpinner = () => (
-<div className="loading-spinner-container">
-    <div className="loading-spinner"></div>
-</div>
+    <div>
+        <div style={{textAlign: "center", fontSize: "20px"}}><br/>Fetching recipes</div>
+        <div className="loading-spinner-container">
+            <div className="loading-spinner"></div>
+        </div>
+    </div>
 );
 
 export default LoadingSpinner;
